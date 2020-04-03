6ec41319a61c22e1902796f76ec89c9f7ccce583
huyduong2792000 <bapzangbo2792000@gmail.com>
2020-04-03 15:35:59 +0700

    update

--- static/app/tasks_employees/CollectionView.js
+++ static/app/tasks_employees/CollectionView.js
@@ -10,18 +10,37 @@ define(function (require) {
 	var Helper = require('app/common/Helpers');
 	// var TemplateHelper = require("app/common/TemplateHelper");
 	var TimeFilterDialogView = require('app/common/filters/TimeFilterDialogView');
-	var TasksCollectionView = require('./TasksCollectionView')
+
 	return Gonrin.CollectionView.extend({
 		template: template,
 		modelSchema: schema,
 		urlPrefix: "/api/v1/",
-		collectionName: "tasks_today",
+		collectionName: "tasks_employees",
+		uiControl: {
+			// orderBy: [{ field: "created_at", direction: "desc" }],
+
+			// fields: [
+			// 	{ field: "task", label: "Công việc", textField: "task_name" },
+			// 	{ field: "employee", label: "Nhân viên", textField: "full_name" },
+			// 	{
+			// 		field: "created_at", label: "Ngày tạo", template: function (rowObj) {
+			// 			return Helper.setDatetime(rowObj.created_at);
+			// 		}
+			// 	}
+			// ],
+			// onRowClick: function (event) {
+			// 	if (event.rowId) {
+			// 		var path = this.collectionName + '/model?id=' + event.rowId;
+			// 		this.getApp().getRouter().navigate(path);
+			// 	}
+			// }
+		},
 
 		render: function () {
 			// this.applyBindings();
 			var self = this;
 			self.eventRegister();
-			let url = self.getApp().serviceURL + '/api/v1/tasks_today'
+			let url = self.getApp().serviceURL + '/api/v1/tasks_employees'
 			self.filterData(url)
 			return this;
 		},
@@ -107,16 +126,7 @@ define(function (require) {
 				},
 			});
 		},
-		calculateTaskTodo:function (group,status) {
-			let total_tasks = group.tasks.length
-			let sum_tasks = 0
-			group.tasks.forEach(function (task,index){
-				if(task.status == status){
-					sum_tasks +=1
-				}
-			})
-			return sum_tasks + '/' + total_tasks
-		},
+
 		loadGridData: function (dataSource) {
 			let total_tasks = dataSource.length
 			var self = this;
@@ -132,35 +142,69 @@ define(function (require) {
 						label: "Công việc ngày hôm nay: " + total_tasks + " việc",
 						template: function (rowObject) {
 
-							var html = `<div class="pb-0 mb-0" style="overflow-x: hidden; background-color: #fff; position: relative;">
-								<div class="row ">
-									<div class="col-lg-4 col-sm-4 col-4">
-										<div class="row mx-0">Tên group</div>
-										<div class="row mx-0 align-middle"><span style="color: #17a2b8;">${rowObject.name ? rowObject.name : ''}</span></div>
-									</div>`;
-							html += `<div class="col-lg-4 col-sm-4 col-4">
-										<div class="pr-1 row">Công việc</div>
-										<div class="pr-1 row text-success">Xong: ${self.calculateTaskTodo(rowObject,1)}</div>
-										<div class="pr-1 row text-secondary">Đang xử lý: ${self.calculateTaskTodo(rowObject,2)}</div>
-										<div class="pr-1 row text-danger">Chưa xong: ${self.calculateTaskTodo(rowObject,0)}</div>	
-									</div>`;
-
-							html += `<div class="col-lg-4 col-sm-4 col-4">
-										<div class="row mr-2">Leader</div>
-										<div class="row pr-1">${rowObject.supervisor.full_name}</div>
-										<div class="row pr-1">Số điện thoại</div>
-										<div class="row pr-1">${rowObject.supervisor.phone_number}</div>
+							var html = `<div style="overflow-x: hidden; background-color: #fff; padding: 5px 5px; position: relative;">
+								<div class="row title">
+									<div class="col-lg-12 col-sm-12 col-12">
+										<div class="float-left">Mã cv: <span style="color: #17a2b8;">${rowObject.task_code ? rowObject.task_code : ''}</span></div>
+										<div class="float-right text-dark">${Helper.utcToLocal(moment.unix(rowObject.start_time).format("YYYY-MM-DDTHH:mm:ss"), "YYYY-MM-DD")}</div>
 									</div>
+								</div>`;
+							html += `<div class="row">
+									<div class="col-lg-12 col-sm-12 col-12">
+										<div class="float-left mr-2">Công việc: </div>
+										<div class="btn-default float-left pl-1 pr-1">${rowObject.task_name}</div>
+										<div class="float-right text-dark" ><p style="color:blue;">${rowObject.priority}</p></div>
+										
+									</div>
+								</div>`;
+
+							html += `<div class="row">
+								<div class="col-lg-12 col-sm-12 col-12">
+									<div class="float-left mr-2">Bắt đầu: </div>
+									<div class="btn-default float-left pl-1 pr-1">${Helper.utcToLocal(moment.unix(rowObject.start_time).format("YYYY-MM-DDTHH:mm:ss"), "HH:mm")}</div>
+									<div class="float-left mr-2">&#160;&#160; Kết thúc: </div>
+									<div class="btn-default float-left pl-1 pr-1">${Helper.utcToLocal(moment.unix(rowObject.end_time).format("YYYY-MM-DDTHH:mm:ss"), "HH:mm")}</div>
 								</div>
 							</div>`;
-							html += `<div id="tasks-collection"></div>`
+							html += `<div class="row">
+							<div class="col-lg-12 col-sm-12 col-12">
+								<div class="float-left mr-2">Người tạo: </div>
+								<div class="btn-default float-left pl-1 pr-1">${rowObject.created_by_name}</div>
+								</div></div>
+								`;
+							html += `<div class="row">
+							<div class="col-lg-12 col-sm-12 col-12">
+								<div class="float-left mr-2">Người nhận: </div>
+								<div class="btn-default float-left pl-1 pr-1">${rowObject.employee_name}</div>
+								</div></div>
+								`;
+							html += `<div class="row">
+								<div class="col-lg-12 col-sm-12 col-12">
+									
+									<div class="float-left mr-2">Điện thoại: </div>
+									<div class="btn-default float-left pl-1 pr-1">${rowObject.employee_phone}</div>
+									`;
+							if (rowObject.status && rowObject.status == 1) {
+								html += `<div class="btn-outline-success float-right pl-2 pr-2 pt-1 pb-1" style="border-radius: 4px;border: 2px solid #28a745; position: absolute; right: 20px; top: -17px; transform: rotate(30deg); font-weight: 600;">Đã xong</div>`;
+							} else if (rowObject.status == 0) {
+								html += `<div class="btn-outline-warning float-right pl-1 pr-1" style="border-radius: 4px;border: 2px solid #ffc107; position: absolute; right: 20px; top: -17px; transform: rotate(30deg); font-weight: 600;">Đang đợi</div>`;
+							} else if (rowObject.status === 2) {
+								html += `<div class="btn-outline-danger float-right pl-2 pr-2 pt-1 pb-1" style="border-radius: 4px;border: 2px solid #dc3545; position: absolute; right: 20px; top: -17px; transform: rotate(30deg); font-weight: 600;">Đang xử lý</div>`;
+							}
+							html += `</div></div>`;
+							html += `</div>
+								</div>
+							</div>`;
 							return html;
 						}
 					}
 				],
 				onRowClick: function (event) {
-					var tasks_collection_view = new TasksCollectionView({viewData:event})
-					tasks_collection_view.render()
+					if (event.rowData.id) {
+						var path = 'tasks/model?id=' + event.rowData.task_uid + '&backcol=' + self.collectionName;
+
+						self.getApp().getRouter().navigate(path);
+					}
 				},
 
 				events: {
